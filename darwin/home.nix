22beee846097246ddba91e56599b2beb477e4304
Daniel Rampelt <drampelt@gmail.com>
2023-02-07 15:17:30 -0500

    Basic neovim setup since vim clipboard issues are frustrating me

--- darwin/home.nix
+++ darwin/home.nix
@@ -130,6 +130,97 @@
   programs.htop.enable = true;
   programs.jq.enable = true;
 
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      let g:mapleader=","
+
+      " Tabs
+      set tabstop=2
+      set softtabstop=2
+      set expandtab
+      set shiftwidth=2
+      set autoindent
+
+      " Display
+      set number
+      set showcmd
+      set cursorline
+      set wildmenu
+      set lazyredraw
+      set showmatch
+      set mouse=a
+
+      " Searching
+      set incsearch
+      set hlsearch
+
+      " Folding
+      set foldenable
+      set foldlevelstart=10
+      set foldnestmax=10
+      set foldmethod=indent
+
+      " Escape
+      inoremap jj <esc>
+
+      """ Colemak-Vim Mappings"""
+      " - k/K is the new n/N.
+      " - s/S is the new i/I ["inSert"].
+      "
+      " - l/L skip to the beginning and end of lines
+      " - Ctrl-l joins lines
+      " - r replaces i as the "inneR" modifier
+
+      " HNEI arrows. Swap 'gn'/'ge' and 'n'/'e'.
+
+      noremap <expr> n (v:count == 0 ? 'gj' : 'j')
+      noremap <expr> e (v:count == 0 ? 'gk' : 'k')
+      noremap i l
+
+      noremap <C-e> <C-u>
+      noremap <C-n> <C-d>
+
+      " Switch panes.
+      nnoremap H <C-w>h
+      nnoremap I <C-w>l
+      nnoremap N <C-w>j
+      nnoremap E <C-w>k
+
+      " Switch buffers.
+      nnoremap <C-i> :tabnext<CR>
+      nnoremap <C-h> :tabprevious<CR>
+
+      " Last search.
+      noremap k n
+      noremap K N
+
+      " _r_ = inneR text objects.
+      onoremap r i
+
+      " Easy mappings for BOL EOL
+      noremap l g^
+      noremap L g$
+
+      """Misc Mappings"""
+      noremap ; :
+      noremap : ;
+      noremap U <C-r>
+      nnoremap <CR> o<Esc>
+      nnoremap <silent> j :noh<CR>
+      noremap <silent> _ :TComment<CR>
+      nnoremap p p=`]
+      noremap s i
+      noremap S I
+      noremap <space> i
+      inoremap <C-e> <C-p>
+      inoremap <C-p> <C-r>
+
+      " Clipboard
+      set clipboard=unnamed
+    '';
+  };
+
   programs.starship.enable = true;
   #programs.starship.package = starshipWorkingPkgs.pkgs.starship;
 
